b34efbfee794a4ec4ae44f2af1553da25dc158c8
Mehfooz-ur-Rehman <mehfoozijaz786@gmail.com>
2021-01-06 22:02:50 +0500

    home page incomplete

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,40 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Navbar, Nav, Container } from "react-bootstrap";
+import logo from "../assets/logo.svg";
 
 export default function Header() {
   return (
-    <Navbar expand="lg" bg="primary" variant="light">
+    <Navbar
+      expand="lg"
+      bg="primary"
+      variant="light"
+      style={{ minHight: "140px" }}
+    >
       <Container>
         <Navbar.Brand href="index.html">
           <img
-            src=""
-            width="30"
-            height="30"
+            src={logo}
+            width="fit-content"
+            height="50"
             className="d-inline-block align-top"
-            alt=""
+            alt="brand-logo"
           />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
           <Nav>
-            <Nav.Link as={Link} to="/" exact className="nav__link">
-              Home
+            <Nav.Link as={Link} to="/coffee-corner" exact className="nav__link">
+              Coffee Corner
             </Nav.Link>
-            <Nav.Link as={Link} to="/link" className="nav__link">
-              Link
+            <Nav.Link as={Link} to="/about" className="nav__link">
+              About Us
             </Nav.Link>
-            <Nav.Link as={Link} to="/link1" className="nav__link">
-              Link
+            <Nav.Link as={Link} to="/blog" className="nav__link">
+              Blog
             </Nav.Link>
-            <Nav.Link as={Link} to="/link2" className="nav__link">
-              Link
+            <Nav.Link as={Link} to="/login" className="nav__link">
+              Login
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
--- src/components/Screens/Screen.js
+++ src/components/Screens/Screen.js
@@ -1,15 +1,32 @@
 import React from "react";
-import { Container, Jumbotron, Button } from "react-bootstrap";
+import {
+  Container,
+  Jumbotron,
+  Button,
+  Form,
+  FormControl,
+} from "react-bootstrap";
 
 export default function Screen() {
   return (
     <Container fluid>
-      <Jumbotron style={{ minHeight: "350px" }}>
-        <Container>
-          <h2>Hello, world!</h2>
+      <Jumbotron
+        className="bg-picture"
+        style={{ minHeight: "560px", marginBottom: "0rem" }}
+      >
+        <Container
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            height: "400px",
+          }}
+        >
+          <h1 style={{ width: "40%", color: "#fff" }}>
+            Discover Top Jobs For Data Professionals
+          </h1>
           <p style={{ width: "50%" }}>
-            This is a simple hero unit, a simple jumbotron-style component for
-            calling extra attention to featured content or information.
+            Don't search for jobs. Find the right fit instead.
           </p>
           <p>
             <Button variant="outline-light">Sign Up</Button>
@@ -17,6 +34,31 @@ export default function Screen() {
         </Container>
       </Jumbotron>
       <Container>
+        <Form.Group className="srearch-card">
+          <Form.Control as="select">
+            <option>Freelance</option>
+            <option>Employeer</option>
+          </Form.Control>
+          <FormControl
+            type="text"
+            placeholder="Keywords"
+            variant="dark"
+            style={{ margin: "0rem 0.5rem" }}
+          />
+          <FormControl
+            type="text"
+            placeholder="Job Location"
+            variant="dark"
+            style={{ margin: "0rem 0.5rem" }}
+          />
+          <Button
+            variant="dark"
+            style={{ margin: "0rem 0.5rem", maxWidth: "450px" }}
+          >
+            Search Now
+          </Button>
+        </Form.Group>
+
         {/* <Card border="primary" style={{ width: '18rem' }}>
     <Card.Header>Header</Card.Header>
     <Card.Body>
