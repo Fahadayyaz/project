97b860db1de92fb0e515e1d3bd8d1bdbef349ad6
fahadayyaz/fahad <fahadayyaz31@gmail.com>
2021-01-12 13:55:57 +0500

    commit

--- src/components/Screens/Blog.js
+++ src/components/Screens/Blog.js
@@ -1,9 +1,36 @@
 import React from "react";
 import "./Blog.scss";
+import {Container,Image,Col,Row,Card} from "react-bootstrap";
 
 class Blog extends React.Component {
   render() {
-    return <div>Blog section</div>;
+    return (
+    <Container fluid>
+      <Container fluid>
+      <Image src="" alt="blog-bg" fluid />
+      </Container>
+      <Container>
+        <Row>Blogs</Row>
+        <Row>
+          <Col>
+          <Card>
+            <Card.Image src=""></Card.Image>
+            <Card.Title>name</Card.Title>
+            <Card.Text>lorem20</Card.Text>
+          </Card>
+          </Col>
+          <Col></Col>
+          <Col></Col>
+        </Row>
+        <Row>
+          <Col></Col>
+          <Col></Col>
+          <Col></Col>
+        </Row>
+      </Container>
+    </Container>
+    );
   }
 }
+
 export default Blog;
